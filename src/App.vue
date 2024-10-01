77ab4615652d501cd617fee20f73cca85ac25d71
Calanfive <camille.lelan@gmail.com>
2024-10-01 18:18:59 +0200

    border's size modified

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ import Card from './components/ui/card/Card.vue'
 <template>
     <h1 class="text-white">{{ msg }}</h1>
   <div class="w-full h-screen bg-stone-900 flex justify-evenly items-center">
-    <div class="w-2/4 h-1/4 border-solid border-2 border-white rounded-lg">
+    <div class="w-2/4 h-1/4 border-solid border border-white rounded-lg">
       <p class="text-white m-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
     </div>
     <Carousel v-slot="{ canScrollNext }" class="relative w-full max-w-xs">
