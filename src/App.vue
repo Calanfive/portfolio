79fceec5fb0b8682de75dac52743bf6ecb546d8a
Calanfive <camille.lelan@gmail.com>
2024-10-01 17:54:42 +0200

    carousel component added

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import Button from './components/ui/button/Button.vue';
 
 defineProps<{ msg: string }>()
@@ -16,5 +16,39 @@ defineProps<{ msg: string }>()
     </div>
   </div>
   <App msg="TITRE HELLOWORD COMPONENT" />
-</template>
+</template> -->
 
+<script setup lang="ts">
+import Button from './components/ui/button/Button.vue';
+
+defineProps<{ msg: string }>()
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, 
+CarouselPrevious } from '@/components/ui/carousel'
+import { Card, CardContent } from '@/components/ui/card'
+</script>
+
+<template>
+    <h1 class="text-white">{{ msg }}</h1>
+  <div class="w-full h-screen bg-stone-900 flex justify-evenly items-center">
+    <div class="w-2/4 h-1/4 border-solid border-2 border-white rounded-lg">
+      <p class="text-white m-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+    </div>
+    <Carousel v-slot="{ canScrollNext }" class="relative w-full max-w-xs">
+    <CarouselContent>
+      <CarouselItem v-for="(_, index) in 5" :key="index">
+        <div class="p-1">
+          <Card>
+            <CardContent class="flex aspect-square items-center justify-center p-6">
+              <span class="text-4xl font-semibold">{{ index + 1 }}</span>
+            </CardContent>
+          </Card>
+        </div>
+      </CarouselItem>
+    </CarouselContent>
+    <CarouselPrevious />
+    <CarouselNext v-if="canScrollNext" />
+  </Carousel>
+  </div>
+  <App msg="TITRE HELLOWORD COMPONENT" />
+
+</template>
