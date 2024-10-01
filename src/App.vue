d0cd7ef813065e264a5dec8808200a94f2f4b3f7
Calanfive <camille.lelan@gmail.com>
2024-10-01 20:03:56 +0200

    tooltip added.

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,30 @@
-<!-- <script setup lang="ts">
-import Button from './components/ui/button/Button.vue';
-
-defineProps<{ msg: string }>()
-
-</script>
-
-<template>
-  <h1 class="text-white">{{ msg }}</h1>
-  <div class="w-full h-screen bg-stone-900 flex justify-evenly items-center">
-    <div class="w-2/4 h-1/4 border-solid border-2 border-white rounded-lg">
-      <p class="text-white m-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-    </div>
-    <div class="w-1/4 h-1/4 border-2 border-white rounded-lg">
-      <p></p>
-    </div>
-  </div>
-  <App msg="TITRE HELLOWORD COMPONENT" />
-</template> -->
-
 <script setup lang="ts">
-import Button from './components/ui/button/Button.vue';
 
 defineProps<{ msg: string }>()
 
+import Button from './components/ui/button/Button.vue';
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, 
 CarouselPrevious } from '@/components/ui/carousel'
-
 import Card from './components/ui/card/Card.vue'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip'
 
 </script>
 
+
+
 <template>
-    <h1 class="text-white">{{ msg }}</h1>
   <div class="w-full h-screen bg-stone-900 flex justify-evenly items-center">
+    <h1 class="text-white bg-stone-900">Camille LE LAN Developpeur</h1>
+
     <div class="w-2/4 h-1/4 border-solid border border-white rounded-lg">
       <p class="text-white m-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
     </div>
+    
     <Carousel v-slot="{ canScrollNext }" class="relative w-full max-w-xs">
     <CarouselContent>
       <CarouselItem v-for="(_, index) in 5" :key="index">
@@ -51,7 +40,17 @@ import Card from './components/ui/card/Card.vue'
     <CarouselPrevious />
     <CarouselNext v-if="canScrollNext" />
   </Carousel>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger as-child>
+        <Button variant="outline">
+          Select project
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>Click for more details</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
   </div>
-  <App msg="Camille LE LAN Developpeur" />
-
 </template>
