31174272ee202122541000b08a38a699151b87e4
Calanfive <camille.lelan@gmail.com>
2024-10-02 04:09:44 +0200

    styles structure modified

--- src/App.vue
+++ src/App.vue
@@ -20,15 +20,14 @@ import { Skeleton } from '@/components/ui/skeleton';
 </script>
 
 <template>
-    <div class="w-full h-screen bg-gradient-to-r from-black via-gray-400 to-black flex justify-evenly items-center">
+    <div class="w-full h-screen bg-gradient-to-b from-black via-blue-800 to-black flex justify-evenly items-center">
         <div class="flex-col w-1/2 items-center">
-            <h1 class=" uppercase font-semibold text-2xl text-white p-1">Camille LE LAN - Developpeur</h1>
+            <h1 class=" uppercase font-semibold text-2xl text-white text-opacity-90 p-1">Camille LE LAN - Developpeur</h1>
             <div
-                class="border-solid border border-white rounded-lg"
-            >
-                <p class="text-white m-2">
+                class="border-solid border border-white rounded-lg">
+                <p class="text-white text-opacity-80 m-2">
                     Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
+                    typesetting industry. Lorem Ipsum has been the industry'sz
                     standard dummy text ever since the 1500s, when an unknown
                     printer took a galley of type and scrambled it to make a
                     type specimen book. It has survived not only five centuries,
@@ -37,31 +36,27 @@ import { Skeleton } from '@/components/ui/skeleton';
                 </p>
             </div>
         </div>
-        <div class="flex-col w-1/4 items-center justify-end p-4">
+        <div class="flex-col justify-items-center-w-1/4 p-2">
             <Carousel
                 v-slot="{ canScrollNext }"
-                class="relative w-full max-w-xs"
-            >
+                class="relative w-full max-w-xs">
                 <CarouselContent>
                     <CarouselItem v-for="(_, index) in 5" :key="index">
                         <div class="p-1">
                             <Card>
                                 <CardContent
-                                    class="flex flex-col aspect-square items-center justify-center p-6"
-                                >
-                                <div class="flex flex-col space-y-2">
-                                    <span
-                                        class="text-white text-4xl font-semibold animate-pulse "
-                                        >{{ index + 1 }}</span
-                                    >
-                                    <div class="space-y-2">
-
-                                    <Skeleton class="h-28 w-[200px]" />
-                                    <p class="text-white animate-pulse">coming soon
-                                    </p>
+                                    class="flex flex-col aspect-square items-center justify-center p-6">
+                                    <div class="flex flex-col space-y-2">
+                                        <span
+                                            class="text-white text-4xl font-semibold animate-pulse "
+                                            >{{ index + 1 }}
+                                        </span>
+                                        <div class="space-y-2">
+                                            <Skeleton class="h-28 w-[200px]" />
+                                            <p class="text-white animate-pulse">coming soon
+                                            </p>
+                                        </div>
                                     </div>
-                                </div>
-
                                 </CardContent>
                             </Card>
                         </div>
@@ -70,16 +65,18 @@ import { Skeleton } from '@/components/ui/skeleton';
                 <CarouselPrevious />
                 <CarouselNext v-if="canScrollNext" />
             </Carousel>
-            <TooltipProvider>
-                <Tooltip>
-                    <TooltipTrigger as-child>
-                        <Button class=" hover:bg-gray-300"  variant="outline"> Select project </Button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                        <p>Click for more details</p>
-                    </TooltipContent>
-                </Tooltip>
-            </TooltipProvider>
+            <div class="flex justify-end p-1 pr-2">
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger as-child>
+                            <Button class="hover:bg-blue-800 hover:text-white"   variant="outline"> Select project </Button>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>Click for more details</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+            </div>
         </div>
     </div>
 </template>
--- src/components/ui/carousel/CarouselPrevious.vue
+++ src/components/ui/carousel/CarouselPrevious.vue
@@ -14,7 +14,7 @@ const { orientation, canScrollPrev, scrollPrev } = useCarousel()
   <Button
     :disabled="!canScrollPrev"
     :class="cn(
-      'touch-manipulation absolute h-8 w-8 rounded-full p-0 hover:bg-gray-300',
+      'touch-manipulation absolute h-8 w-8 rounded-full p-0 hover:bg-blue-800 hover:text-white',
       orientation === 'horizontal'
         ? '-left-12 top-1/2 -translate-y-1/2'
         : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
